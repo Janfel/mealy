b7ebb28e346e46d15817ad13a84ca758c9008977
Jan Felix Langenbach <o.hase3@gmail.com>
2019-01-14 16:54:37 +0100

    Fix License

--- tests/test_mealy.py
+++ tests/test_mealy.py
@@ -2,7 +2,7 @@
 
 # Copyright (C) 2019 Jan Felix Langenbach
 #
-# This file is part of tests.
+# This file is part of mealy.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
